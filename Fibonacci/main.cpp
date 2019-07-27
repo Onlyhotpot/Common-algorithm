cce37cc053d731d2727952a6c9953537f06e4ef4
wxy <wxy@hust.com>
2019-07-27 18:02:16 +0800

    update

--- Fibonacci/main.cpp
+++ Fibonacci/main.cpp
@@ -6,6 +6,6 @@ int main()
     std::cout << "Please input n: ";
     int n;
     std::cin >> n;
-    std::cout << "Fibonacci(n) is " << Fibonacci(n) << std::endl;
+    std::cout << "Fibonacci(" << n << ") is " << Fibonacci(n) << std::endl;
     return 0;
 }
