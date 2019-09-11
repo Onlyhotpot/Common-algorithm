f98eb4ee65f0085a4b0cb7b7264ebaf76cc70705
wxy <wxy@hust.cn>
2019-09-11 19:27:41 +0800

    update

--- Fibonacci/Fibonacci.cpp
+++ Fibonacci/Fibonacci.cpp
@@ -5,9 +5,9 @@ int Fibonacci(int n)
     // 递归解法（不推荐)
     // return n == 1 || n == 2 ? 1 : Fibonacci(n-1)+Fibonacci(n-2);
 
-    int a(1),b(1);
-    int result(1);
-    for (int i(2); i <= n; i++){
+    int a = 1,b = 1;
+    int result = 1;
+    for (int i = 2; i < n; i++){
         result = a + b;
         a = b;
         b = result;
