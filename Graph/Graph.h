5ae657dd83e17ad7c58ad979e8e4bca3c583ade8
wxy <wxy@csy.com>
2019-11-29 14:59:43 +0800

    update

--- Graph/Graph.h
+++ Graph/Graph.h
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include "queue.h"
+
+#ifndef GRAPH_H
+#define GRAPH_H
+
 /* 图的邻接表表示法 */
+
 #define MaxVertexNum 100    /* 最大顶点数设为100 */
 #define false 0
 #define true 1
@@ -46,32 +51,38 @@ struct GNode
 typedef PtrToGNode LGraph;
 
 /* 以邻接表方式存储的图类型 */
-LGraph CreateGraph( int VertexNum )   /* 初始化一个有VertexNum个顶点但没有边的图 */
+LGraph CreateGraph(int VertexNum)   /* 初始化一个有VertexNum个顶点但没有边的图 */
 {
     Vertex V;
     LGraph Graph;
-    Graph = (LGraph)malloc( sizeof(struct GNode) ); /* 建立图 */
+    Graph = (LGraph)malloc(sizeof(struct GNode)); /* 建立图 */
     Graph->Nv = VertexNum;
-    Graph->Ne = 0;     /* 初始化邻接表头指针 */
+    Graph->Ne = 0;
+    /* 初始化邻接表头指针 */
     /* 注意：这里默认顶点编号从0开始，到(Graph->Nv - 1) */
-    for (V=0; V<Graph->Nv; V++)
+    for (V = 0; V < Graph->Nv; V++)
         Graph->G[V].FirstEdge = NULL;
     return Graph;
 }
 
-void InsertEdge( LGraph Graph, Edge E )
+void InsertEdge(LGraph Graph, Edge E)    /* 插入边 <V1, V2> */
 {
-    PtrToAdjVNode NewNode;          /* 插入边 <V1, V2> */
+    PtrToAdjVNode NewNode;
+
     /* 为V2建立新的邻接点 */
     NewNode = (PtrToAdjVNode)malloc(sizeof(struct AdjVNode));
     NewNode->AdjV = E->V2;
-    NewNode->Weight = E->Weight;     /* 将V2插入V1的表头 */
+    NewNode->Weight = E->Weight;
+    /* 将V2插入V1的表头 */
     NewNode->Next = Graph->G[E->V1].FirstEdge;
-    Graph->G[E->V1].FirstEdge = NewNode;              /* 若是无向图，还要插入边 <V2, V1> */
+    Graph->G[E->V1].FirstEdge = NewNode;
+
+    /* 若是无向图，还要插入边 <V2, V1> */
     /* 为V1建立新的邻接点 */
     NewNode = (PtrToAdjVNode)malloc(sizeof(struct AdjVNode));
     NewNode->AdjV = E->V1;
-    NewNode->Weight = E->Weight;     /* 将V1插入V2的表头 */
+    NewNode->Weight = E->Weight;
+    /* 将V1插入V2的表头 */
     NewNode->Next = Graph->G[E->V2].FirstEdge;
     Graph->G[E->V2].FirstEdge = NewNode;
 }
@@ -81,64 +92,60 @@ LGraph BuildGraph()
     LGraph Graph;
     Edge E;
     Vertex V;
-    int Nv, i;
+    int Nv;
     scanf("%d", &Nv);   /* 读入顶点个数 */
     Graph = CreateGraph(Nv); /* 初始化有Nv个顶点但没有边的图 */
     scanf("%d", &(Graph->Ne));   /* 读入边数 */
     if ( Graph->Ne != 0 )   /* 如果有边 */
     {
-        E = (Edge)malloc( sizeof(struct ENode) ); /* 建立边结点 */
+        E = (Edge)malloc(sizeof(struct ENode)); /* 建立边结点 */
         /* 读入边，格式为"起点 终点 权重"，插入邻接矩阵 */
-        for (i=0; i<Graph->Ne; i++)
+        for (int i = 0; i < Graph->Ne; i++)
         {
             scanf("%d %d %d", &E->V1, &E->V2, &E->Weight);              /* 注意：如果权重不是整型，Weight的读入格式要改 */
-            InsertEdge( Graph, E );
+            InsertEdge(Graph, E);
         }
     }
 
     /* 如果顶点有数据的话，读入数据 */
-    for (V=0; V<Graph->Nv; V++)
+    for (V = 0; V < Graph->Nv; V++)
         scanf(" %c", &(Graph->G[V].Data));
     return Graph;
 }
 
 /* 邻接表存储 - 拓扑排序算法 */
-bool TopSort( LGraph Graph, Vertex TopOrder[] )
+bool TopSort(LGraph Graph, Vertex* TopOrder)    /* 对Graph进行拓扑排序,  TopOrder[]顺序存储排序后的顶点下标 */
 {
-    /* 对Graph进行拓扑排序,  TopOrder[]顺序存储排序后的顶点下标 */
-    int Indegree[MaxVertexNum], cnt;
+    int Indegree[MaxVertexNum];
     Vertex V;
     PtrToAdjVNode W;
-    Queue Q = CreateQueue( Graph->Nv );
+    Queue Q = CreateQueue(Graph->Nv);
     /* 初始化Indegree[] */
-    for (V=0; V<Graph->Nv; V++)
+    for (V = 0; V < Graph->Nv; V++)
         Indegree[V] = 0;
     /* 遍历图，得到Indegree[] */
-    for (V=0; V<Graph->Nv; V++)
-        for (W=Graph->G[V].FirstEdge; W; W=W->Next)
-            Indegree[W->AdjV]++; /* 对有向边<V, W->AdjV>累计终点的入度 */
+    for (V = 0; V < Graph->Nv; V++)
+        for (W = Graph->G[V].FirstEdge; W; W = W->Next)
+            ++Indegree[W->AdjV]; /* 对有向边<V, W->AdjV>累计终点的入度 */
     /* 将所有入度为0的顶点入列 */
-    for (V=0; V<Graph->Nv; V++)
-        if ( Indegree[V]==0 )
+    for (V = 0; V < Graph->Nv; V++)
+        if (Indegree[V] == 0)
             enqueue(Q, V);
     /* 下面进入拓扑排序 */
-    cnt = 0;
-    while( !isEmpty(Q) )
+    int cnt = 0;
+    while(!isEmpty(Q))
     {
         V = dequeue(Q); /* 弹出一个入度为0的顶点 */
         TopOrder[cnt++] = V; /* 将之存为结果序列的下一个元素 */
         /* 对V的每个邻接点W->AdjV */
-        for ( W=Graph->G[V].FirstEdge; W; W=W->Next )
-            if ( --Indegree[W->AdjV] == 0 )/* 若删除V使得W->AdjV入度为0 */
+        for (W = Graph->G[V].FirstEdge; W; W = W->Next)
+            if (--Indegree[W->AdjV] == 0)/* 若删除V使得W->AdjV入度为0 */
                 enqueue(Q, W->AdjV); /* 则该顶点入列 */
     } /* while结束*/
-    if ( cnt != Graph->Nv )
+    if (cnt != Graph->Nv)
         return false; /* 说明图中有回路, 返回不成功标志 */
     else
         return true;
 }
 
-int main()
-{
-    return 0;
-}
+#endif // GRAPH_H
