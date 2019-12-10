f7d14893010c78b524b564ba439f515bda477c52
wxy <wxy@csy.com>
2019-12-10 23:55:40 +0800

    update

--- JosephCircle/Josephus.c
+++ JosephCircle/Josephus.c
@@ -1,45 +1,37 @@
 #include<stdio.h>
-#include<time.h>
 #include<stdlib.h>
 
 int Josephus(int n, int m);
 
 int main()
 {
-    while (1)
-    {
-        printf("Please input the n and m: ");
-        int n, m;
-        scanf("%d%d", &n, &m);
-        int res;
-        clock_t start, end;
+    printf("Please input the n and m: ");
+    int n, m;
+    scanf("%d%d", &n, &m);
+    int res;
 
-        start = clock();
-        for (int i = 0; i < 1000000; ++i)
-            res = Josephus(n, m);
-        end = clock();
+    res = Josephus(n, m);
 
-        printf("The one who is alive is %d.\n", res);
-        printf("Execution time of 1000000 times was %lf seconds.\n",
-                (long double)(1.0*(end - start)/CLOCKS_PER_SEC));
-    }
+    printf("The one who is alive is %d.\n", res);
+
     system("pause");
     return 0;
 }
 
 int Josephus(int n, int m)
 {
-    int* link = (int*)malloc(n*sizeof(int));
+    int* link = (int*)malloc((n + 1) * sizeof(int));
     for (int i = 0; i < n; ++i)
-        link[i] = (i + 1)%n;
-    int header = n - 1;
+        link[i] = i + 1;
+    link[n] = 1;
+    int header = n;
     while (link[header] != header)
     {
-        int temp = m%(n++);
-        for (int i = 1; i < temp; ++i)
+        for (int i = 1; i < m; ++i)
             header = link[header];
+        printf("%d ", link[header]);
         link[header] = link[link[header]];
     }
     free(link);
-    return header + 1;
+    return header;
 }
--- Sort/radix_sort.c
+++ Sort/radix_sort.c
@@ -19,34 +19,23 @@ int main()
         for (int i = 0; i < length; ++i)
         {
             nums[i] = rand() % 100000;
-            //printf("%d ", nums[i]);
+            printf("%d ", nums[i]);
         }
-        // printf("\n");
+        printf("\n");
 
         printf("The radix you want to sorted by: ");
         int radix;
         scanf("%d", &radix);
 
-        clock_t start, end;
-        start = clock();
-        for (int i = 0; i < 1000; ++i)
-        {
-            radixSort(nums, length, radix);
-        }
-        end = clock();
-
-        printf("Execution time was %.3lf seconds\n", (long double)1.0*(end - start) / CLOCKS_PER_SEC);
-        /*
+        radixSort(nums, length, radix);
         //打印出排序后的数组
         printf("RadixSort:\n");
         for (int i = 0; i < length; ++i)
             printf("%d ", nums[i]);
         printf("\n");
-        */
 
         free(nums);
     }
-
 	system("pause");
 	return 0;
 }
@@ -55,10 +44,8 @@ void radixSort(int* nums, int length, int radix)
 {
     int max = nums[0];
     for (int i = 1; i < length; ++i)
-    {
         if (nums[i] > max)
             max = nums[i];
-    }
 
     // 获取基数为radix的最大数字的长度
     int max_size = 0;
@@ -74,9 +61,7 @@ void radixSort(int* nums, int length, int radix)
         {
             temp[i] = nums[i];
             for (int j = 1; j < cnt; ++j)
-            {
                 temp[i] /= radix;
-            }
             temp[i] %= radix;
         }
 
@@ -88,28 +73,20 @@ void radixSort(int* nums, int length, int radix)
 
         // 进行基本的计数
         for (int i = 0; i < length; ++i)
-        {
             ++countArray[temp[i]];
-        }
 
         // 对计数数组进行变形，后面元素等于前面元素之和
         for (int i = 1; i < 10; ++i)
-        {
             countArray[i] += countArray[i-1];
-        }
 
         int *sortedArray = (int*)malloc(length * sizeof(int));
         // 倒序遍历原数组，从计数数组中找到正确的位置，输出到结果数组
         for (int i = length - 1; i >= 0; --i)
-        {
             sortedArray[--countArray[temp[i]]] = nums[i];
-        }
 
         // 将排序后的数组复制会原数组
         for (int i = 0; i < length; ++i)
-        {
             nums[i] = sortedArray[i];
-        }
 
         free(temp);
         free(countArray);
